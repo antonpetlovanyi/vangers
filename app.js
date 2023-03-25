96220dbc31288c066277aed558787c76e1cb1c4c
Tony <avtoexpert.03@gmail.com>
2023-03-25 19:04:41 +0200

    make massive and function with two parameters

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-document.body.innerHTML += "жиес зогружен!";
+let mars = { x: 100, y: 100 };
+let sun = { x: 0, y: 500 };
+let particles = [mars, sun];
+
+particles.forEach(printParticle);
+
+function printParticle(p, index) {
+    const id = 'planet-' + index;
+    document.body.innerHTML += `<img id="${id}" src="particle.jpg">`;
+    p.element = document.querySelector('#' + id);
+    p.element.style.position = 'absolute';
+    p.element.style.left = p.x + 'px';
+    p.element.style.top = p.y + 'px';
+}
+
